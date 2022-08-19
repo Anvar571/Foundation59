d9203dc5140ca65bd1a4bb3e6ca097f149b51f77
Anvar571 <abdurashidovanvar52@gmail.com>
2022-08-19 16:38:53 +0500

    vscodeda tugirlandi

--- Tubmi.py
+++ Tubmi.py
@@ -1,8 +1,7 @@
- def Tubmi(son):
- 2     if son<2:
- 3         return False
- 4     for i in range(2,son//2):
- 5         if not son%i:
- 6             return False
- 7     return True
- 8     
+def Tubmi(son):
+        if son < 2:
+            return False
+        for i in range(2, son//2):
+            if not son % i:
+                return False
+        return True
