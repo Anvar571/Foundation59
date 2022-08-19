82a8f92d7af945c1f9d04acf2f002cef17d26b74
fazliddin0506 <fazliddinx0506@gmail.com>
2022-08-19 17:06:45 +0500

    men qoshtim

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ def sayHello(str):
 sayHello("Anvar")
 
 
-print('my new branch fazliddin ...')
-
-print('men Abbos akamni branchidaman')
-
-print("boshqa branchdan o'zgartrdimm")
+#dfjsadfjosadfklsafd
